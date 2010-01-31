218aec9bc69b79e09632dc57c3bf75368e67ca14
sourcewalker <sourcewalker@fb4ad34c-0dce-11df-bbc3-97105669a493>
2010-01-31 01:47:48 +0000

    Fixed for IE.

--- trunk/closure-draw/bin/compiledemo.sh
+++ trunk/closure-draw/bin/compiledemo.sh
@@ -9,10 +9,12 @@ cd $BIN_DIR
 
 for jsfile in $DEMOS_DIR/*.js; do
 	basename=${jsfile%.js}
-	$PYTHON $CALCDEPS -i $basename.js \
-	    -p $CLOSURE_LIB_DIR/goog -p $CLOSURE_DRAW_DIR/lib \
-	    -o compiled -c $CLOSURE_COMPILER \
-	    -f "--compilation_level=${OPTIMIZATION}_OPTIMIZATIONS" \
-		-f "--create_source_map=$basename-min-map" \
-		> $basename-min.js
+	if [ ! -z ${basename%%*-min} ]; then
+		$PYTHON $CALCDEPS -i $basename.js \
+			-p $CLOSURE_LIB_DIR/goog -p $CLOSURE_DRAW_DIR/lib \
+			-o compiled -c $CLOSURE_COMPILER \
+			-f "--compilation_level=${OPTIMIZATION}_OPTIMIZATIONS" \
+			-f "--create_source_map=$basename-min-map" \
+			> $basename-min.js
+	fi
 done
--- trunk/closure-draw/lib/demos/gadget.js
+++ trunk/closure-draw/lib/demos/gadget.js
@@ -6,12 +6,20 @@ goog.require('goog.ui.ToolbarSeparator');
 goog.require('closuredraw');
 
 function initialize() {
+  var width = 0, height = 512;
+  if(document.documentElement.scrollWidth) {
+	width = document.documentElement.scrollWidth;
+  } else {
+	width = document.body.scrollWidth;
+  }
+
   if(goog.userAgent.IE) {
-	goog.dom.$('warning').style.display = 'block';
+	goog.dom.$('svgtext-div').style.display = 'block';
+	height = 256;
   }
 
   var outer  = goog.dom.$('canvas');
-  var canvas = new closuredraw.Widget(outer.clientWidth, 512);
+  var canvas = new closuredraw.Widget(width, height);
   canvas.render(outer);
 
   var toolbar = canvas.getToolbar();
@@ -19,9 +27,12 @@ function initialize() {
   toolbar.addChildAt(saveBtn, 0, true);
   toolbar.addChildAt(new goog.ui.ToolbarSeparator(), 1, true);
   goog.events.listen(saveBtn, goog.ui.Component.EventType.ACTION, function(e) {
-	var svg = canvas.exportSVG().documentElement;
-	var url = goog.string.urlEncode(goog.dom.xml.serialize(svg));
-	window.open('data:image/svg+xml,' + url, null);
+	var svg = goog.dom.xml.serialize(canvas.exportSVG().documentElement);
+	if(goog.userAgent.IE) {
+	  goog.dom.setTextContent(goog.dom.$('svgtext'), svg);
+	} else {
+	  window.open('data:image/svg+xml,' + goog.string.urlEncode(svg), null);
+	}
   });
 }
 goog.exportSymbol('initialize', initialize, window);
